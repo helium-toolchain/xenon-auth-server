092529efc6f8b487f690975344a704f310ab5515
ExaInsanity <nicolaiandorfer@outlook.com>
2021-09-17 21:07:54 +0200

    properly handle bad requests

--- XenonAuthServer/Controllers/MsaController.cs
+++ XenonAuthServer/Controllers/MsaController.cs
@@ -1,6 +1,7 @@
 namespace Xenon.Auth.Server.Controllers;
 
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
 
 public class MsaController : Controller
 {
@@ -8,9 +9,19 @@ public class MsaController : Controller
 	[Route("/msa")]
 	public ActionResult GetMsaApplicationId()
 	{
-		if (String.IsNullOrWhiteSpace(XenonAuthServer.MsaApplicationId))
+		if(String.IsNullOrWhiteSpace(XenonAuthServer.MsaApplicationId))
 		{
 			return this.NotFound();
+		}
+
+		if(!this.Request.Headers.TryGetValue("X-Client-Type", out StringValues vs))
+		{
+			return this.BadRequest();
+		}
+
+		else if(vs[0] != "Xenon")
+		{
+			return this.Forbid();
 		}
 
 		return this.Ok(XenonAuthServer.MsaApplicationId);
